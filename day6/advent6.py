332601fa01c2a1cf115038b6a5f9e2a932acf71f
caggursoyMac <caggursoy93@icloud.com>
2020-12-07 21:59:55 +0100

    Day 6 done!

--- day6/advent6.py
+++ day6/advent6.py
@@ -1,5 +1,38 @@
 ##### Part one #####
-inpFile = open('./day5-input.txt')
+inpFile = open('./day6-input.txt')
 lineList = []
-for line in inpFile.read().split('\n'):
+yesList = ['a','b','c','x','y','z']
+yesNum = 0
+yesNumList = []
+for line in inpFile.read().split('\n\n'):
     lineList.append(line)
+
+for line in lineList:
+    line = list(dict.fromkeys(line))
+    for char in line:
+        if char != '\n':
+            yesNum += 1
+    yesNumList.append(yesNum)
+    yesNum = 0
+print('Answer for part one is:',sum(yesNumList))
+
+###### Part two #####
+from collections import Counter
+total = 0
+with open('day6-input.txt') as fp:
+    lines=fp.readlines()
+groups = []
+group = []
+for question in lines:
+    if question!="\n":
+        group.append(question.split("\n")[0])
+    else:
+        groups.append(group)
+        group=[]
+groups.append(group)
+for group in groups:
+    group_size = len(group)
+    counts = Counter(''.join(group))
+    counts = Counter(list(counts.values()))[group_size]
+    total+=counts
+print('Answer for part two is:', total)
